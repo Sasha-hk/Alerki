c4f09b65d442d0f88523f26c40f1d8a939231704
Sasha-hk <sachapos50@gmail.com>
2022-01-08 17:00:43 +0200

    Make some configuration

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const express = require('express')
-const router = require('./router.js')
-
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const dbConnect = require('./models/connect')
 
 
 const PORT = process.env.PORT || 3000
@@ -9,12 +10,17 @@ const app = express()
 
 // middlewares
 app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
-// router
-app.use('/', router)
+// routers
 
-const start = () => {
+
+const start = async () => {
     try {
+        await dbConnect(() => {
+            console.log('Connected to database')
+        })
         app.listen(PORT, () => {
             console.log(`Server started! http://localhost:${PORT}`)
         })
