bf11983d33ce7a92f60c0b1c7db4f65e76687317
Sasha-hk <sachapos50@gmail.com>
2022-01-10 17:48:40 +0200

    Update colors

--- docker/colors.sh
+++ docker/colors.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-RED=`tput setaf 1`
-GREE=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-PINK=`tput setaf 5`
-CYAN=`tput setaf 6`
-None=`tput sgr0`
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PINK='\033[0;35m'
+CYAN='\033[0;36m'
+None='\033[0m'
