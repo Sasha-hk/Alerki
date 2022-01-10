4bf8b04e906dd768c0cd1e0218d66dc860f28ee3
Sasha-hk <sachapos50@gmail.com>
2022-01-10 19:46:41 +0200

    Create models

---
+++ api/models/AppointmentModels.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const AppointmentModel = sequelize.define(
+        'AppointmentModel',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            appointmentTime: {
+                type: DataTypes.DATE,
+            },
+        },
+        {
+            tableName: 'Appointments',
+        }
+    )
+
+    AppointmentModel.associate = (models) => {
+        AppointmentModel.belongsTo(models.ServiceModel, {
+            foreignKey: 'service',
+            onDelete: 'CASCADE',
+            allowNull:true,
+            defaultValue:null,
+        })
+    }
+
+    return AppointmentModel
+}
