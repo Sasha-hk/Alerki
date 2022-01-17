d00126b604ae2bd41cf2535f2d1570c2e4e7ba1c
Sasha-hk <sachapos50@gmail.com>
2022-01-17 20:31:46 +0200

    Update build-production.sh script

--- docker/build-production.sh
+++ docker/build-production.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 
-. ./docker/colors.sh
-
 if [ -f "./docker/.env.production" ]; then 
     while read line; do
         export $line;
     done < ./docker/.env.production
 
-    docker-compose -f ./docker/docker-compose.production.yml build
-
 else 
     echo "${RED}\n - [!] You need to create .env.production file${None}"
 
 fi
+
+docker-compose -f ./docker/docker-compose.production.yml build
