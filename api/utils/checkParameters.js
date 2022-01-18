483fbd50845482cf2ff1f3a6bb6268ba26d27672
Sasha-hk <sachapos50@gmail.com>
2022-01-19 01:14:25 +0200

    Patch utility checkParamers

--- api/utils/checkParameters.js
+++ api/utils/checkParameters.js
@@ -1,25 +1,51 @@
 const APIError = require('../exception/APIError')
 
 // cause error
-function makeError(errors) {
+function causeError(errors) {
     throw APIError.BadRequestError(errors)
 }
 
-// make errors
+// make errors list
 function prepareErrors(parameters) {
-    let errors = []
-    Object.keys(parameters).forEach(p => {
-        errors.push(`${p} is required`)
-    })
+    // check if parameters if array or object    
+    if (Array.isArray(parameters)) {
+        parameters.filter(p => p)
+        
+        causeError(parameters)
+    }
+    else if (typeof parameters === 'object' && parameters !== null) {
+        let errors = []
 
-    makeError(errors)
+        Object.keys(parameters).forEach(p => {
+            if (!parameters[p]) {
+                errors.push(`${p} is required`)
+            }
+        })
+
+        causeError(errors)
+    }
+
+
 }
 
 // check parameters function
 module.exports = (parameters) => {
-    parameters.forEach(p => {
-        if (!p) {
-            prepareErrors(parameters)
-        }
-    })
+    // check if parametser is array or object
+    if (Array.isArray(parameters)) {
+        parameters.forEach(p => {
+            if (!p) {
+                prepareErrors(parameters)
+            }
+        })
+    }
+    else if (typeof parameters === 'object' && parameters !== null) {
+        Object.keys(parameters).forEach(p => {
+            if (!parameters[p]) {
+                prepareErrors(parameters)
+            }
+        })
+    }
+    else {
+        throw Error('Parameters it is neither an array nor an object')
+    }
 }
