2e1223dfce33379ddca880788aeb8aba48527f3d
Sasha-hk <sachapos50@gmail.com>
2022-01-18 16:19:49 +0200

    Resolve #96

---
+++ api/__tests__/auth.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest')
+const app = require('../app')
+
+
+describe('Test authentication', () => {
+    describe('register', () => {
+        test('few users with correct parammeters', () => {
+
+        })
+
+        test('with correct parammeters', async () => {
+
+        })
+
+        test('with incorrect parammeters', async () => {
+
+        })
+    })
+
+    describe('login', () => {
+        test('with correct parameters', async () => {
+
+        })
+
+        test('with incorrect parameters', async () => {
+            
+        })
+    })
+
+    describe('refresh', () => {
+        test('with correct refreshToken', async () => {
+
+        })
+
+        test('with incorrect refreshToken', async () => {
+            
+        })
+    })
+
+    describe('logout', () => {
+        test('just logout', async () => {
+
+        })
+    }) 
+})
---
+++ api/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    globalSetup: './__tests__/setup.js',
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+    roots: ['__tests__'],
+}
