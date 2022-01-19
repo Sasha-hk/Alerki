30ea88838c8b2c40b09e4a7246d717973acd2b57
Sasha-hk <sachapos50@gmail.com>
2022-01-19 02:26:35 +0200

    Resolve #102

---
+++ api/utils/oneExists.js
@@ -0,0 +1,35 @@
+const APIError = require('../exception/APIError')
+
+// cause error
+function causeError(errors) {
+    throw APIError.BadRequestError(errors)
+}
+
+// make errors list
+function prepareError(parameter) {
+    let errors = []
+
+    Object.keys(parameter).forEach(p => {
+        if (!parameter[p]) {
+            errors.push(`${p} is required`)
+        }
+    })
+
+    causeError(errors)
+}
+
+// check parameters function
+module.exports = (parameter) => {
+    // check if parameters is object
+    if (typeof parameter === 'object' && parameter !== null) {
+        if (!parameter) {
+            prepareError(parameter)
+        }
+        else {
+            return true
+        }
+    }
+    else {
+        throw Error('Parameters is not an object')
+    }
+}
