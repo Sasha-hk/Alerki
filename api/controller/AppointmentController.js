f9ee1f20b4abd412177487125c11f3425c3a3333
Sasha-hk <sachapos50@gmail.com>
2022-01-23 00:06:04 +0200

    Define few appointment controllers

--- api/controller/AppointmentController.js
+++ api/controller/AppointmentController.js
@@ -1,5 +1,84 @@
 class AppointmentController {
-    
+    async detail(req, res, next) {
+        try {
+
+        }
+        catch (e) {
+            res.status(e.status || 500).json(e.errrors)
+        }
+    }
+
+    async clientFor(req, res, next) {
+        try {
+
+        }
+        catch (e) {
+            res.status(e.status || 500).json(e.errrors)
+        }
+    }
+
+    async clientToday(req, res, next) {
+        try {
+
+        }
+        catch (e) {
+            res.status(e.status || 500).json(e.errrors)
+        }
+    }
+
+    async workerFor(req, res, next) {
+        try {
+
+        }
+        catch (e) {
+            res.status(e.status || 500).json(e.errrors)
+        }
+    }
+
+    async workerToday(req, res, next) {
+        try {
+
+        }
+        catch (e) {
+            res.status(e.status || 500).json(e.errrors)
+        }
+    }
+
+    async create(req, res, next) {
+        try {
+
+        }
+        catch (e) {
+            res.status(e.status || 500).json(e.errrors)
+        }
+    }
+
+    async clientCancel(req, res, next) {
+        try {
+
+        }
+        catch (e) {
+            res.status(e.status || 500).json(e.errrors)
+        }
+    }
+
+    async workerCancel(req, res, next) {
+        try {
+
+        }
+        catch (e) {
+            res.status(e.status || 500).json(e.errrors)
+        }
+    }
+
+    async workerConfirm(req, res, next) {
+        try {
+
+        }
+        catch (e) {
+            res.status(e.status || 500).json(e.errrors)
+        }
+    }
 }
 
 
