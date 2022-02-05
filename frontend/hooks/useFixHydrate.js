bba6dfd310c623f4be3127f74811f550c842f162
Petryk Oleksandr <sachapos50@gmail.com>
2022-02-06 01:28:21 +0200

    Update useFixHydrate hook

--- frontend/hooks/useFixHydrate.js
+++ frontend/hooks/useFixHydrate.js
@@ -1,18 +1,18 @@
 import {useEffect, useState} from 'react'
 
 // crutch to fix "React hydration error"
-export default (toHydrate) => {
+export default (toHydrate, deps = []) => {
     const [hydrate, setHydrate] = useState(null)
 
     if (typeof body == 'function') {
         useEffect(() => {
             setHydrate(toHydrate)
-        }, [])
+        }, deps)
     }
     else {
         useEffect(() => {
             setHydrate(toHydrate)
-        }, [])
+        }, deps)
     }
 
     return hydrate
