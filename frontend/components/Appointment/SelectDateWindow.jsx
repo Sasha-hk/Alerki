71a3b3cdef653e2ac854ccf8dd43f63729991a86
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-01 00:14:41 +0200

    Few little updates

--- frontend/components/Appointment/SelectDateWindow.jsx
+++ frontend/components/Appointment/SelectDateWindow.jsx
@@ -1,15 +1,12 @@
-import { useEffect, useState, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux' 
 import Modal from '../Modal/Modal'
 import Button from '../UI/Button/Button'
-import Input from '../UI/Input/Input'
-import masterActions from '../../store/actions/masterActions.js'
-import SelectService from './Select/SelectService'
-import SelectMaster from './Select/SelectMaster'
 import ModalHeading from '../Modal/ModalHeading'
 import ModalContent from '../Modal/ModalContent'
-import SearchInput from '../Modal/UI/SearchInput'
 import Calendar from '../UI/Calendar/Calendar'
+import AvailableDay from '../UI/Calendar/DayView/Available'
+import NotAvailableDay from '../UI/Calendar/DayView/NotAvailable'
+import AnotherMonth from '../UI/Calendar/DayView/AnotherMonth'
 
 
 const SelectDataWindow = ({
--- frontend/components/UI/Calendar/Calendar.jsx
+++ frontend/components/UI/Calendar/Calendar.jsx
@@ -1,17 +1,10 @@
-import { useEffect, useRef } from 'react'
-import Available from './DayView/Available'
-import NotAvailable from './DayView/NotAvailable'
-import AnotherMonth from './DayView/AnotherMonth'
 import cls from './calendar.module.css'
 
 
 const Calendar = ({children, ...props}) => {
-  const calendarElement = useRef() 
-
   return (
     <div
       {...props}
-      ref={calendarElement}
       className={[cls.calendar, props.className].join(' ')}
     >
       {children}  
