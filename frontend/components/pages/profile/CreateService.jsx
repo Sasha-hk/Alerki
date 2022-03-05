be2a082384dc581aadee0b3b79c4ea062acb9cf6
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-05 15:44:02 +0200

    Update master working time UI and update service view

--- frontend/components/pages/profile/CreateService.jsx
+++ frontend/components/pages/profile/CreateService.jsx
@@ -1,18 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import Router, { useRouter } from 'next/router'
-import Link from 'next/link'
 import useTranslation from 'next-translate/useTranslation'
-import getT from 'next-translate/getT'
 import userActions from '../../../store/actions/userActions.js'
 import profileActions from '../../../store/actions/profileActions.js'
 import Button from '../../UI/Button/Button'
-import Input from '../../UI/Input/Input'
-import Select from '../../UI/Select/Select'
-import Modal from '../../Modal/Modal'
 import InputServiceWindow from './InputServcieWindow'
-import Toggle from '../../UI/Toggle/Toggle'
-import cls from '../../../styles/pages/profile.module.css'
 
 
 const defaultServiceData = {
--- frontend/components/pages/profile/UpdateService.jsx
+++ frontend/components/pages/profile/UpdateService.jsx
@@ -1,18 +1,9 @@
-import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import Router, { useRouter } from 'next/router'
-import Link from 'next/link'
 import useTranslation from 'next-translate/useTranslation'
-import getT from 'next-translate/getT'
 import userActions from '../../../store/actions/userActions.js'
 import profileActions from '../../../store/actions/profileActions.js'
 import Button from '../../UI/Button/Button'
-import Input from '../../UI/Input/Input'
-import Select from '../../UI/Select/Select'
-import Modal from '../../Modal/Modal'
 import InputServcieWindow from './InputServcieWindow'
-import Toggle from '../../UI/Toggle/Toggle'
-import cls from '../../../styles/pages/profile.module.css'
 
 
 const ManageServiceWIndow = ({showModal, setShowModal, serviceData, setServiceData}) => {
