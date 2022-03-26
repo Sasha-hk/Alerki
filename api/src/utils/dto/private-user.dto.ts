d60ddf62a4f9a0b8d6e790eb88d7099e2e12b95b
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-26 23:57:45 +0200

    feat: create private user dto

---
+++ api/src/utils/dto/private-user.dto.ts
@@ -0,0 +1,41 @@
+import IUser from '../../interfaces/db/models/user.interface';
+import { UserModel } from '../../db/models';
+
+interface IPrivateUserDto extends Omit<IUser, 'password'> {}
+
+/**
+ * This data transfer object intended to transfer data that belongs only current user
+ */
+class PrivateUserDto implements IPrivateUserDto {
+  id: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  pictureID: number;
+  profileType: 'client' | 'master';
+  clientID: number;
+  masterID: number;
+  banned: boolean;
+
+  /**
+   * Private user DTO constructor
+   * @param {UserModel} user User date
+   */
+  constructor(user: UserModel) {
+    this.id = user.id;
+    this.username = user.username;
+    this.firstName = user.firstName;
+    this.lastName = user.lastName;
+    this.email = user.email;
+    this.phoneNumber = user.phoneNumber;
+    this.pictureID = user.pictureID;
+    this.profileType = user.profileType;
+    this.clientID = user.clientID;
+    this.masterID = user.masterID;
+    this.banned = user.banned;
+  }
+}
+
+export default PrivateUserDto;
