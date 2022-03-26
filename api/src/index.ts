c920ee87ec5501565e4ce0bcf149f8b182e42d60
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-27 00:56:47 +0200

    feat: remove set middlewared block

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,8 @@
 import App from './app';
-import AuthController from './controllers/auth.controller';
 import Database from './db/connect';
 
 const db = new Database();
 const app = new App();
-
-app.setControllers([
-  new AuthController(),
-]);
 
 db.connect();
 app.listen();
