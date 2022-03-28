fe1fbe435f4a5720bd881c00292e1c86d31b79fa
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-28 17:45:23 +0300

    ci: create few tests

--- api/src/__tests__/integration/auth.test.ts
+++ api/src/__tests__/integration/auth.test.ts
@@ -1,22 +1,66 @@
 import request from 'supertest';
 import App from '../../app';
+import AuthError from '../../errors/auth.error';
+import IError from '../../interfaces/error.interface';
 
 const APP = new App();
 const app = APP.getApp();
+
+const user: object = {
+  username: 'user',
+  email: 'user@user.com',
+  profileType: 'client',
+  password: 'user',
+};
+
+async function wrapAuthError(
+  actions: () => any,
+  callback: (e: IError | any) => any,
+) {
+  try {
+    await actions();
+
+    throw Error('Expected to catch AuthError');
+  } catch (e: IError | any) {
+    callback(e);
+  }
+}
 
 describe('Authentication functionality', () => {
   describe('test register endpoint', () => {
     it('should register user with correct data', async () => {
       const res = await request(app)
         .post('/auth/register')
-        .send({
-          username: 'Username',
-          email: 'example@example.com',
-          password: '1234',
-          profileType: 'client',
-        });
+        .send(user);
 
       expect(res.status).toEqual(200);
+    });
+
+    it('should not register user without any data', async () => {
+      const r = await request(app)
+        .post('/auth/register')
+        .send({});
+
+      expect(r.statusCode).toEqual(400);
+    });
+
+    it('should return 400 code due to incorrect email', async () => {
+      const r = await request(app)
+        .post('/auth/register')
+        .send({
+          ...user,
+          email: 'bad@email',
+        });
+
+      expect(r.statusCode).toEqual(400);
+    });
+
+    it('should not register user with the same data', async () => {
+      const r = await request(app)
+        .post('/auth/register')
+        .send(user);
+
+      expect(r.statusCode).toEqual(400);
     });
   });
 });
