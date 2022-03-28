1289cb922f98506e47419e68619cf427fbceb51f
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-28 21:07:58 +0300

    feat: update middleware

--- api/src/middlewares/isAuthenticated.ts
+++ api/src/middlewares/isAuthenticated.ts
@@ -4,20 +4,20 @@ import IError from '../interfaces/error.interface';
 import AuthService, { ITokenizeUser } from '../services/auth.service';
 
 export interface AuthRequest extends Request {
-  token?: ITokenizeUser | null;
+  token?: ITokenizeUser;
 }
 
 export default async function isAuthenticated(req: AuthRequest, res: Response, next: NextFunction) {
   try {
-    const accessToken = req.cookies;
+    const { accessToken } = req.cookies;
 
     if (!accessToken) {
       throw AuthError.Unauthorized();
     }
 
-    const decoded = AuthService.validateAccessToken(accessToken) as ITokenizeUser;
+    const decoded = AuthService.validateAccessToken(accessToken);
 
-    req.token = decoded || null;
+    req.token = decoded;
 
     next();
   } catch (e: IError | any) {
