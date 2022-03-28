6707cd7eb16f400d07ae091713a48cf4e77f49ed
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-28 17:45:06 +0300

    fix: get port variable name

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -19,7 +19,7 @@ const DBConfig: IDBConfig = {
     password: process.env.DB_DEV_PASSWORD,
     database: process.env.DB_DEV_DATABASE,
     host: process.env.DB_DEV_HOST,
-    port: process.env.DB_TEST_PORT,
+    port: process.env.DB_DEV_PORT,
     dialect: process.env.DB_DEV_DIALECT,
   },
   test: {
@@ -35,7 +35,7 @@ const DBConfig: IDBConfig = {
     password: process.env.DB_PROD_PASSWORD,
     database: process.env.DB_PROD_DATABASE,
     host: process.env.DB_PROD_HOST,
-    port: process.env.DB_TEST_PORT,
+    port: process.env.DB_PROD_PORT,
     dialect: process.env.DB_PROD_DIALECT,
   },
 };
