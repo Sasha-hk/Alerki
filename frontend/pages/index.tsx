dbba0b1b1426b55ad974fc11a9542c715685c197
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-29 20:57:14 +0300

    feat: create frontend

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  console.log(1);
+
+  return (
+    <div>
+      Under development...
+    </div>
+  );
+};
+
+export default Home;
