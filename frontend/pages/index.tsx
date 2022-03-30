a96143095e1c5004470b25189df175695b7c1d64
Petryk Oleksandr <sachapos50@gmail.com>
2022-03-31 01:02:43 +0300

    feat: craete metatags for frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
 /* eslint max-len: 0 */
 import { useState } from 'react';
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   const [progress, setProgress] = useState(0);
@@ -15,18 +16,30 @@ const Home: NextPage = () => {
   }, 1000);
 
   return (
-    <div className="block">
-      <div className="welcome-block">
-        <span>Welcome on </span><b>Alerki</b>
-      </div>
-      <small className="loading">Under development{progressView}</small>
+    <>
+      <Head>
+        <meta
+          name="keywords"
+          content="Appointment service, appointment management for masters, appointment service for client, make an appointment"
+        />
+        <meta
+          name="description"
+          content="Alerki is a useful, handy &amp; modern tool for plan &amp; make an appointment for craftsmen &amp; client"
+        />
+      </Head>
+      <div className="block">
+        <div className="welcome-block">
+          <span>Welcome on </span><b>Alerki</b>
+        </div>
+        <small className="loading">Under development{progressView}</small>
 
-      <div className="github-block">
-        <a href="https://github.com/Sasha-hk/Alerki">
-          <b>GitHub</b> <span>Repository</span>
-        </a>
+        <div className="github-block">
+          <a href="https://github.com/Sasha-hk/Alerki">
+            <b>GitHub</b> <span>Repository</span>
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
