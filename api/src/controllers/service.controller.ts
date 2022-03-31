47f50649baf6d56b24ba1e77e1e39779debf6590
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-01 01:43:02 +0300

    feat: probably finish services controller

--- api/src/controllers/service.controller.ts
+++ api/src/controllers/service.controller.ts
@@ -1,7 +1,9 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response } from 'express';
+import Validator from '../utils/validator';
 import Controller from '../interfaces/controller.interface';
 import IError from '../interfaces/error.interface';
-import { ServiceModel } from '../db/models';
+import ServiceError from '../errors/service.error';
+import ServiceService from '../services/service.service';
 
 interface IServiceController extends Controller {
   getServices(req: Request, res: Response): any;
@@ -11,16 +13,37 @@ interface IServiceController extends Controller {
  * Services controller
  */
 class ServiceController implements IServiceController {
-  public path = '/service';
+  public path = '/services';
   public router = Router();
 
   constructor() {
-    console.log(123);
+    this.router.get(`${this.path}`, this.getServices);
   }
 
   async getServices(req: Request, res: Response) {
     try {
-      const l = 12;
+      const {
+        name,
+      } = req.query;
+
+      Validator({
+        all: [
+          {
+            value: name,
+            name: 'name',
+            type: 'string',
+            maxLength: 30,
+          },
+        ],
+      });
+
+      const foundServices = await ServiceService.findAllByName(name as string);
+
+      if (!foundServices) {
+        throw ServiceError.NotFound();
+      }
+
+      res.json(foundServices);
     } catch (e: IError | any) {
       res.status(e?.status || 500).json(e?.error);
     }
