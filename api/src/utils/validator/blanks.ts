8eca8b6e34611b54f6d8d25e3e4c1351b20cf3c6
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-04 23:16:29 +0300

    feat: update blanks

--- api/src/utils/validator/blanks.ts
+++ api/src/utils/validator/blanks.ts
@@ -1,34 +1,38 @@
-import { IValidateItem } from '../../interfaces/validator.interface';
+import IValidateFields from './validator.interface';
 
-export const usernameField = (username: string): IValidateItem => ({
-  value: username,
-  name: 'username',
-  type: 'string',
-  maxLength: 20,
-  minLength: 4,
+export const usernameField = (username: string): IValidateFields => ({
+  username: {
+    value: username,
+    type: 'string',
+    maxLength: 20,
+    minLength: 4,
+  },
 });
 
-export const emailField = (email: string): IValidateItem => ({
-  value: email,
-  name: 'email',
-  type: 'string',
-  pattern: /^\w+@\w+\.\w+/,
-  maxLength: 319,
-  minLength: 4,
+export const emailField = (email: string): IValidateFields => ({
+  email: {
+    value: email,
+    type: 'string',
+    pattern: /^\w+@\w+\.\w+/,
+    maxLength: 319,
+    minLength: 4,
+  },
 });
 
-export const passwordField = (password: string): IValidateItem => ({
-  value: password,
-  name: 'password',
-  required: true,
-  type: 'string',
-  minLength: 4,
-  maxLength: 100,
+export const passwordField = (password: string): IValidateFields => ({
+  password: {
+    value: password,
+    required: true,
+    type: 'string',
+    minLength: 4,
+    maxLength: 100,
+  },
 });
 
-export const profileTypeField = (profileType: string): IValidateItem => ({
-  value: profileType,
-  name: 'profileType',
-  type: 'string',
-  pattern: /(client|master)/,
+export const profileTypeField = (profileType: string): IValidateFields => ({
+  profileType: {
+    value: profileType,
+    type: 'string',
+    pattern: /(client|master)/,
+  },
 });
