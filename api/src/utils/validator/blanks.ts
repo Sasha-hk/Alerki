aa4ba99c6a0c2a76f92fb43768b363b1fcebff54
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-05 00:31:08 +0300

    feat: update blanks

--- api/src/utils/validator/blanks.ts
+++ api/src/utils/validator/blanks.ts
@@ -1,38 +1,42 @@
 import IValidateFields from './validator.interface';
 
-export const usernameField = (username: string): IValidateFields => ({
+export const usernameField = (username: string | any, options?: object): IValidateFields => ({
   username: {
     value: username,
     type: 'string',
-    maxLength: 20,
+    pattern: /^\w+$/,
     minLength: 4,
+    maxLength: 20,
+    ...options,
   },
 });
 
-export const emailField = (email: string): IValidateFields => ({
+export const emailField = (email: string | any, options?: object): IValidateFields => ({
   email: {
     value: email,
     type: 'string',
     pattern: /^\w+@\w+\.\w+/,
-    maxLength: 319,
     minLength: 4,
+    maxLength: 319,
+    ...options,
   },
 });
 
-export const passwordField = (password: string): IValidateFields => ({
+export const passwordField = (password: string | any, options?: object): IValidateFields => ({
   password: {
     value: password,
-    required: true,
     type: 'string',
     minLength: 4,
     maxLength: 100,
+    ...options,
   },
 });
 
-export const profileTypeField = (profileType: string): IValidateFields => ({
+export const profileTypeField = (profileType: string | any, options?: object): IValidateFields => ({
   profileType: {
     value: profileType,
     type: 'string',
     pattern: /(client|master)/,
+    ...options,
   },
 });
