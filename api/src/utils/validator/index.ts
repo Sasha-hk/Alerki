538219c3c9a68cebd5a35baee01ef199db3e4aa5
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-05 00:31:58 +0300

    feat: add static method to validate and throw error

--- api/src/utils/validator/index.ts
+++ api/src/utils/validator/index.ts
@@ -1,6 +1,7 @@
 import IValidateFields, { IValidateField } from './validator.interface';
 import ValidationError from '../../errors/validation.error';
 import checks, { FullCheck, PartialCheck } from './check';
+export * as blanks from './blanks';
 
 export interface IErrorPool {
   [key: string]: string;
@@ -98,7 +99,7 @@ class Validator implements IValidator {
     }
 
     if (throwError) {
-      this.throwError(errorPool);
+      return Validator.throwError(errorPool);
     }
   }
 
@@ -106,13 +107,56 @@ class Validator implements IValidator {
    * Static method to validate params, without user checks
    * @param fields Params to validate
    */
-  static validate(fields: IValidateFields) {}
+  static validate(fields: IValidateFields) {
+    const fieldKeys = Object.keys(fields);
+    const fieldKeysLength = fieldKeys.length;
+
+    const partialCheckKeys = Object.keys(checks.partialChecks);
+    const partialChecksLength = partialCheckKeys.length;
+    const fullCheckKeys = Object.keys(checks.fullChecks);
+    const fullChecksLength = fullCheckKeys.length;
+    const partialChecks = checks.partialChecks as IPartialChecks;
+    const fullChecks = checks.fullChecks as IFullChecks;
+
+    const errorPool: IErrorPool = {};
+    let throwError = false;
+
+    // Partial checks
+    for (let i = 0; i < fieldKeysLength; i++) {
+      for (let j = 0; j < partialChecksLength; j++) {
+        // Check if validation type specified in field
+        if (partialCheckKeys[j] in fields[fieldKeys[i]]) {
+          if (partialChecks[partialCheckKeys[j]](fields[fieldKeys[i]], errorPool, fieldKeys[i])) {
+            throwError = true;
+            continue;
+          }
+        }
+      }
+    }
+
+    // Full checks
+    for (let i = 0; i < fullChecksLength; i++) {
+      let wasChecked = false;
+      for (let j = 0; j < fieldKeysLength; j++) {
+        if (!wasChecked && fullCheckKeys[i] in fields[fieldKeys[j]]) {
+          wasChecked = true;
+          if (fullChecks[fullCheckKeys[i]](fields, errorPool)) {
+            throwError = true;
+          }
+        }
+      }
+    }
+
+    if (throwError) {
+      return Validator.throwError(errorPool);
+    }
+  }
 
   /**
    * Throw error
    * @param errorPool Errors
    */
-  throwError(errorPool: IErrorPool) {
+  static throwError(errorPool: IErrorPool) {
     throw new ValidationError(400, 'Validation error', {
       error: {
         message: 'validation error',
