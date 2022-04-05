6e9f4f533b778a6d43d6dd29548e6158400bcbfb
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-06 00:31:12 +0300

    fix: return index.ts view

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,21 +1,12 @@
-// D import App from './app';
-// import Database from './db/connect';
+import App from './app';
+import Database from './db/connect';
 
-// async function start() {
-//   const db = new Database();
-//   const app = new App();
+async function start() {
+  const db = new Database();
+  const app = new App();
 
-//   await db.connect();
-//   app.listen();
-// }
+  await db.connect();
+  app.listen();
+}
 
-// start();
-
-const r = RegExp(/\w{1,305}.@\w{1,10}.\w{1,4}/g);
-
-const rs = r.source;
-
-console.log(rs.replace(/\{([^}])+\}/g, '{0,1}'));
-
-console.log(rs.match(/\{([^}])+\}/g)?.length);
-
+start();
