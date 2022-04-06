7a9495f4771bc9228af77c12e6653d03fdac5748
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-07 00:06:51 +0300

    ci: disable `totest` utility

--- api/src/__tests__/unit/totest.test.ts
+++ api/src/__tests__/unit/totest.test.ts
@@ -1,37 +1,37 @@
 import Totest from '../../utils/totest';
 
 it('just a test', async () => {
-  const test = new Totest(
-    {
-      request: {
-        body: {
-          username: {
-            type: 'string',
-            required: true,
-            minLength: 4,
-            maxLength: 30,
-          },
-          email: {
-            type: 'string',
-            required: true,
-            pattern: /\w{1,305}.@\w{1,10}.\w{1,4}/,
-            minLength: 5,
-            maxLength: 319,
-          },
-        },
-      },
-    },
-    data => {
-      console.log(data);
+  // L const test = new Totest(
+  //   {
+  //     request: {
+  //       body: {
+  //         username: {
+  //           type: 'string',
+  //           required: true,
+  //           minLength: 4,
+  //           maxLength: 30,
+  //         },
+  //         email: {
+  //           type: 'string',
+  //           required: true,
+  //           pattern: /\w{1,305}.@\w{1,10}.\w{1,4}/,
+  //           minLength: 5,
+  //           maxLength: 319,
+  //         },
+  //       },
+  //     },
+  //   },
+  //   data => {
+  //     console.log(data);
 
-      return {
-        body: {},
-        cookies: {},
-        status: 0,
-      };
-    },
-    compare => {
-      console.log(compare);
-    },
-  ).test();
+  //     return {
+  //       body: {},
+  //       cookies: {},
+  //       status: 0,
+  //     };
+  //   },
+  //   compare => {
+  //     console.log(compare);
+  //   },
+  // ).test();
 });
