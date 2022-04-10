88a60d48cd90f2f6fa34d93c842183fb1d504738
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-10 20:36:22 +0300

    feat: refactoring user picture service

--- api/src/services/user-picture.service.ts
+++ api/src/services/user-picture.service.ts
@@ -1,13 +1,27 @@
 import { UserPictureModel } from '../db/models';
 import UserPictureError from '../errors/user-picture.error';
 
-interface IUserPictureService {
-  findPicture(id: string): Promise<UserPictureModel | null>;
-  savePicture(picture: Buffer): Promise<UserPictureModel>;
-  updatePicture(id: string, picture: Buffer): Promise<any>;
+interface ISavePicture {
+  id?: string,
+  picture: Buffer,
 }
 
+interface IUserPictureService {
+  findPicture(id: string): Promise<UserPictureModel | null>;
+  createPicture(picture: Buffer): Promise<UserPictureModel>;
+  updatePicture(id: string, picture: Buffer): Promise<any>;
+  savePicture({ id, picture }: ISavePicture): Promise<UserPictureModel>;
+}
+
+/**
+ * User picture service
+ */
 class UserPictureService implements IUserPictureService {
+  /**
+   * Find picture by ID
+   * @param id Picture ID
+   * @returns UserPictureModel
+   */
   async findPicture(id: string) {
     return UserPictureModel.findOne({
       raw: true,
@@ -17,12 +31,23 @@ class UserPictureService implements IUserPictureService {
     });
   }
 
-  async savePicture(picture: Buffer) {
+  /**
+   * Create record with user picture
+   * @param picture Picture
+   * @returns UserPictureModel
+   */
+  async createPicture(picture: Buffer) {
     return UserPictureModel.create({
       picture,
     });
   }
 
+  /**
+   * Update picture
+   * @param id Picture ID
+   * @param picture Picture
+   * @returns UserPictureModel
+   */
   async updatePicture(id: string, picture: Buffer) {
     const candidate = await this.findPicture(id);
 
@@ -42,6 +67,26 @@ class UserPictureService implements IUserPictureService {
     );
 
     return this.findPicture(id);
+  }
+
+  /**
+   * Create of update picture
+   * @param options Picture and id if exists
+   * @returns UserPictureModel
+   */
+  async savePicture({ id, picture }: ISavePicture) {
+    // If id exists try to update picture else create new
+    if (id) {
+      const candidate = await this.findPicture(id);
+
+      if (candidate) {
+        return this.updatePicture(id, picture);
+      }
+
+      return this.updatePicture(id, picture);
+    }
+
+    return this.createPicture(picture);
   }
 }
 
