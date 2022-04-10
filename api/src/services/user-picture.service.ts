baeb077d5eb5c97b35892aba20d804e47b00880d
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-10 14:17:11 +0300

    fix: create user picture service

---
+++ api/src/services/user-picture.service.ts
@@ -0,0 +1,52 @@
+import { UserPictureModel } from '../db/models';
+import UserPictureError from '../errors/user-picture.error';
+
+interface ICreateService {
+  name: string;
+}
+
+interface IUserPictureService {
+  findPicture(id: string): Promise<UserPictureModel | null>;
+  savePicture(picture: Buffer): Promise<UserPictureModel>;
+  updatePicture(id: string, picture: Buffer): Promise<any>;
+}
+
+class UserPictureService implements IUserPictureService {
+  async findPicture(id: string) {
+    return UserPictureModel.findOne({
+      raw: true,
+      where: {
+        id,
+      },
+    });
+  }
+
+  async savePicture(picture: Buffer) {
+    return UserPictureModel.create({
+      picture,
+    });
+  }
+
+  async updatePicture(id: string, picture: Buffer) {
+    const candidate = await this.findPicture(id);
+
+    if (!candidate) {
+      throw UserPictureError.NotFound();
+    }
+
+    await UserPictureModel.update(
+      {
+        picture,
+      },
+      {
+        where: {
+          id,
+        },
+      },
+    );
+
+    return this.findPicture(id);
+  }
+}
+
+export default new UserPictureService();
