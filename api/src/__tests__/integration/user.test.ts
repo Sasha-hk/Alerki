1a7ad4572749c694b8d6844f0a5480d1f06be7a3
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-16 22:35:42 +0300

    ci: write tests for user endpoints block

--- api/src/__tests__/integration/user.test.ts
+++ api/src/__tests__/integration/user.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import axios from 'axios';
 import App from '../../app';
+import serviceService from '../../services/service.service';
 import getCookies from '../../utils/get-cookies';
 
 const APP = new App();
@@ -152,6 +153,144 @@ describe('Services functionality', () => {
 
       expect(r.status).toEqual(200);
       expect(r.body.master.biography).toEqual(biography);
+    });
+
+    it('should update master start / end work time', async () => {
+      const r = await request(app)
+        .patch('/user/master')
+        .set('Cookie', [
+          'accessToken=' + user.accessToken,
+          'refreshToken=' + user.refreshToken,
+        ])
+        .send({
+          startTime: 1,
+          endTime: 12000,
+        });
+
+      expect(r.status).toEqual(200);
+      expect(r.body.master.startTime).toEqual(1);
+      expect(r.body.master.endTime).toEqual(12000);
+    });
+
+    it('should update master delay before / after appointment', async () => {
+      const r = await request(app)
+        .patch('/user/master')
+        .set('Cookie', [
+          'accessToken=' + user.accessToken,
+          'refreshToken=' + user.refreshToken,
+        ])
+        .send({
+          delayBefore: 1000,
+          delayAfter: 500,
+        });
+
+      expect(r.status).toEqual(200);
+      expect(r.body.master.delayBefore).toEqual(1000);
+      expect(r.body.master.delayAfter).toEqual(500);
+    });
+
+    it('should update master weekend days', async () => {
+      const r = await request(app)
+        .patch('/user/master')
+        .set('Cookie', [
+          'accessToken=' + user.accessToken,
+          'refreshToken=' + user.refreshToken,
+        ])
+        .send({
+          weekendDays: {
+            monday: true,
+            wednesday: true,
+          },
+        });
+
+      expect(r.status).toEqual(200);
+    });
+
+    it('update all master data', async () => {
+      const biography = 'New programmer biography';
+      const r = await request(app)
+        .patch('/user/master')
+        .set('Cookie', [
+          'accessToken=' + user.accessToken,
+          'refreshToken=' + user.refreshToken,
+        ])
+        .send({
+          biography,
+          startTime: 10,
+          endTime: 20,
+          delayBefore: 4,
+          delayAfter: 1,
+        });
+
+      expect(r.status).toEqual(200);
+      expect(r.body.master.biography).toEqual(biography);
+      expect(r.body.master.startTime).toEqual(10);
+      expect(r.body.master.endTime).toEqual(20);
+      expect(r.body.master.delayBefore).toEqual(4);
+      expect(r.body.master.delayAfter).toEqual(1);
+    });
+  });
+
+  describe('test master service manipulation endpoints', () => {
+    let service = { id: '' };
+
+    let masterService = { id: '' };
+
+    it('should create or get service', async () => {
+      service = await serviceService.createOrGet({ name: 'new service name' });
+    });
+
+    it('should create master service', async () => {
+      const r = await request(app)
+        .post('/user/master/service')
+        .set('Cookie', [
+          'accessToken=' + user.accessToken,
+          'refreshToken=' + user.refreshToken,
+        ])
+        .send({
+          serviceID: service.id,
+          duration: 100,
+          price: 100,
+          currency: 'UAN',
+          locationLat: 10.1,
+          locationLng: 10.2,
+        });
+
+      expect(r.status).toEqual(200);
+      expect(r.body.duration).toEqual(100);
+      expect(r.body.price).toEqual(100);
+      expect(r.body.currency).toEqual('UAN');
+      expect(r.body.locationLat).toEqual(10.1);
+      expect(r.body.locationLng).toEqual(10.2);
+      masterService = r.body;
+    });
+
+    it('should update master service', async () => {
+      const r = await request(app)
+        .patch('/user/master/service/' + masterService.id)
+        .set('Cookie', [
+          'accessToken=' + user.accessToken,
+          'refreshToken=' + user.refreshToken,
+        ])
+        .send({
+          duration: 300,
+        });
+
+      console.log(r.body);
+
+      expect(r.status).toEqual(200);
+      expect(r.body.duration).toEqual(300);
+    });
+
+    it('should delete master service', async () => {
+      const r = await request(app)
+        .delete('/user/master/service/' + masterService.id)
+        .set('Cookie', [
+          'accessToken=' + user.accessToken,
+          'refreshToken=' + user.refreshToken,
+        ]);
+
+      expect(r.status).toEqual(200);
     });
   });
 
