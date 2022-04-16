ffddec1de8cb87a9bd4d55202193a8157d903a83
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-16 20:12:37 +0300

    feat: update init migration

--- api/prisma/migrations/20220416171220_init/migration.sql
+++ api/prisma/migrations/20220416171220_init/migration.sql
@@ -9,8 +9,8 @@ CREATE TABLE "MasterWeekendDays" (
     "wednesday" BOOLEAN NOT NULL DEFAULT false,
     "thursday" BOOLEAN NOT NULL DEFAULT false,
     "friday" BOOLEAN NOT NULL DEFAULT false,
-    "saturday" BOOLEAN NOT NULL DEFAULT false,
-    "sunday" BOOLEAN NOT NULL DEFAULT false,
+    "saturday" BOOLEAN NOT NULL DEFAULT true,
+    "sunday" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -22,8 +22,11 @@ CREATE TABLE "MasterProfiile" (
     "id" TEXT NOT NULL,
     "biography" VARCHAR(100),
     "weekendDaysID" VARCHAR(36),
+    "startTime" INTEGER,
+    "endTime" INTEGER,
+    "delayBefore" INTEGER,
+    "delayAfter" INTEGER,
     "available" BOOLEAN NOT NULL DEFAULT true,
-    "startWorking" TIMETZ(6),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -94,6 +97,65 @@ CREATE TABLE "Services" (
     CONSTRAINT "Services_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "MasterServices" (
+    "id" TEXT NOT NULL,
+    "currency" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "locationLat" DOUBLE PRECISION NOT NULL,
+    "locationLng" DOUBLE PRECISION NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "serviceID" VARCHAR(36) NOT NULL,
+    "masterID" VARCHAR(36) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "MasterServices_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MasterSchedule" (
+    "id" TEXT NOT NULL,
+    "masterID" VARCHAR(36) NOT NULL,
+    "startTime" INTEGER NOT NULL,
+    "endTime" INTEGER NOT NULL,
+    "weekend" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "MasterSchedule_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Appointments" (
+    "id" TEXT NOT NULL,
+    "masterServiceID" VARCHAR(36) NOT NULL,
+    "masterID" VARCHAR(36) NOT NULL,
+    "clientID" VARCHAR(36) NOT NULL,
+    "clientConfirm" BOOLEAN NOT NULL DEFAULT true,
+    "masterConfirm" BOOLEAN NOT NULL DEFAULT false,
+    "masterCancel" BOOLEAN NOT NULL DEFAULT false,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Appointments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notifications" (
+    "id" TEXT NOT NULL,
+    "actorID" VARCHAR(36) NOT NULL,
+    "notifierID" VARCHAR(36) NOT NULL,
+    "entity" INTEGER NOT NULL,
+    "entityType" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Notifications_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "MasterProfiile_weekendDaysID_key" ON "MasterProfiile"("weekendDaysID");
 
@@ -121,6 +183,30 @@ CREATE UNIQUE INDEX "AuthData_userID_key" ON "AuthData"("userID");
 -- CreateIndex
 CREATE UNIQUE INDEX "Services_available_key" ON "Services"("available");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "MasterServices_masterID_key" ON "MasterServices"("masterID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MasterServices_serviceID_key" ON "MasterServices"("serviceID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MasterSchedule_masterID_key" ON "MasterSchedule"("masterID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Appointments_masterID_key" ON "Appointments"("masterID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Appointments_clientID_key" ON "Appointments"("clientID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Appointments_masterServiceID_key" ON "Appointments"("masterServiceID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Notifications_actorID_key" ON "Notifications"("actorID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Notifications_notifierID_key" ON "Notifications"("notifierID");
+
 -- AddForeignKey
 ALTER TABLE "MasterProfiile" ADD CONSTRAINT "MasterProfiile_weekendDaysID_fkey" FOREIGN KEY ("weekendDaysID") REFERENCES "MasterWeekendDays"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
@@ -135,3 +221,27 @@ ALTER TABLE "Users" ADD CONSTRAINT "Users_pictureID_fkey" FOREIGN KEY ("pictureI
 
 -- AddForeignKey
 ALTER TABLE "AuthData" ADD CONSTRAINT "AuthData_userID_fkey" FOREIGN KEY ("userID") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MasterServices" ADD CONSTRAINT "MasterServices_masterID_fkey" FOREIGN KEY ("masterID") REFERENCES "MasterProfiile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MasterServices" ADD CONSTRAINT "MasterServices_serviceID_fkey" FOREIGN KEY ("serviceID") REFERENCES "Services"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MasterSchedule" ADD CONSTRAINT "MasterSchedule_masterID_fkey" FOREIGN KEY ("masterID") REFERENCES "MasterProfiile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointments" ADD CONSTRAINT "Appointments_masterID_fkey" FOREIGN KEY ("masterID") REFERENCES "MasterProfiile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointments" ADD CONSTRAINT "Appointments_clientID_fkey" FOREIGN KEY ("clientID") REFERENCES "ClientProfiile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointments" ADD CONSTRAINT "Appointments_masterServiceID_fkey" FOREIGN KEY ("masterServiceID") REFERENCES "MasterServices"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notifications" ADD CONSTRAINT "Notifications_actorID_fkey" FOREIGN KEY ("actorID") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notifications" ADD CONSTRAINT "Notifications_notifierID_fkey" FOREIGN KEY ("notifierID") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
