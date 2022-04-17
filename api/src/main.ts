05386bc85ff0a3c7dc0d82c4589cc2a834114253
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-17 23:37:02 +0300

    feat: create Prisma service and Swagger docs

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+  console.log(process.env.DATABASE_URL);
+  const PORT = process.env.PORT || 5000;
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
 }
 
-bootstrap();
+start();
