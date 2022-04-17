e7a74da6ee0467f7d1dbc15c2c9e7b13cc796cb2
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-17 20:54:28 +0300

    feat: change project structure to next

--- api/jest.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  globalSetup: './src/__tests__/setup.ts',
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-  roots: ['./src/__tests__'],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
--- api/src/index.ts
+++
@@ -1,8 +0,0 @@
-import prisma from './prisma';
-import App from './app';
-
-const app = new App();
-
-app.listen();
-
-export default prisma;
