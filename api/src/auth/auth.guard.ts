89bd1beaba612632dd711ea9cd827d9395084983
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-19 02:46:13 +0300

    feat: create auth guard

---
+++ api/src/auth/auth.guard.ts
@@ -0,0 +1,39 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+
+/**
+ * Authentication guard
+ *
+ * Check if user authenticated
+ */
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+    try {
+      const accessToken = req.cookies?.accessToken;
+
+      if (!accessToken) {
+        throw new UnauthorizedException({ message: 'user not authorized' });
+      }
+
+      const verified = this.authService.verifyAccessToken(accessToken);
+
+      console.log(accessToken);
+
+      req.accessToken = verified;
+      return true;
+    } catch (e) {
+      throw new UnauthorizedException({ message: 'user not authorized' });
+    }
+  }
+}
