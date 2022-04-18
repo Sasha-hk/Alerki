da5ea15f539a74aa9b58390b51a2d4c3e726d4d7
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-18 18:02:21 +0300

    feat: create dot for register and log-in

--- api/src/auth/dto/log-in.dto.ts
+++ api/src/auth/dto/log-in.dto.ts
@@ -1,51 +1,40 @@
-import Prisma from '@prisma/client';
 import {
   MinLength,
   MaxLength,
   IsString,
-  IsEnum,
   Matches,
+  ValidateIf,
 } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-interface IRegister extends Pick<Prisma.User, 'username' | 'email' | 'profileType' | 'password'> {}
-
-export class RegisterDto implements IRegister {
+export class LogInDto {
   @ApiProperty({
     description: 'Username',
     minLength: 4,
-    maxLength: 4,
+    maxLength: 15,
     type: 'string',
   })
   @IsString()
   @MinLength(4)
   @MaxLength(15)
-  readonly username: string;
+  @ValidateIf(o => !o.email || o.username)
+  readonly username?: string;
 
   @ApiProperty({
     description: 'Email',
     minLength: 5,
     maxLength: 319,
     type: 'string',
-    pattern: '/\\w+@\\w+.\\w+/',
   })
   @IsString()
   @MinLength(5)
   @MaxLength(319)
   @Matches(/\w+@\w+.\w+/)
-  readonly email: string;
+  @ValidateIf(o => !o.username || o.email)
+  readonly email?: string;
 
   @ApiProperty({
-    description: 'User profile type',
-    type: 'string',
-    enum: ['master', 'client'],
-  })
-  @IsString()
-  @IsEnum(['master', 'client'])
-  readonly profileType: Prisma.ProfileType;
-
-  @ApiProperty({
-    description: 'Email',
+    description: 'Password',
     minLength: 4,
     maxLength: 50,
     type: 'string',
