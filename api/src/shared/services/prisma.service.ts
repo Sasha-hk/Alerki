adfa6bbb8aff070bc21ea9511202382ba5d5f4d3
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-19 02:46:58 +0300

    feat: update Prisma service

--- api/src/shared/services/prisma.service.ts
+++ api/src/shared/services/prisma.service.ts
@@ -1,6 +1,9 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+/**
+ * Prisma service
+ */
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
