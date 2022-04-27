bc0e5714e6af20ad26ef8269c70f6a0bbccdbf09
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-27 23:46:05 +0300

    fix: udpate session dto

--- api/src/auth/dto/session.dto.ts
+++ api/src/auth/dto/session.dto.ts
@@ -1,9 +1,17 @@
 import Prisma from '@prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
 
-interface ISession extends Pick<Prisma.Session, 'deviceName' | 'userID' | 'createdAt' | 'updatedAt'> {}
+interface ISession extends Pick<Prisma.Session, 'id' | 'deviceName' | 'createdAt' | 'updatedAt'> {}
 
 export class SessionDto implements ISession {
+  @ApiProperty({
+    description: 'Session ID',
+    maxLength: 36,
+    type: 'string',
+    example: '123e4567-e89b-12d3-a456-426614174847',
+  })
+  readonly id: string;
+
   @ApiProperty({
     description: 'Device name',
     maxLength: 30,
@@ -11,13 +19,6 @@ export class SessionDto implements ISession {
     example: 'IPhone XR',
   })
   readonly deviceName: string;
-
-  @ApiProperty({
-    description: 'User ID',
-    type: 'string',
-    example: '123e4567-e89b-12d3-a456-426614174000',
-  })
-  readonly userID: string;
 
   readonly createdAt: Date;
 
