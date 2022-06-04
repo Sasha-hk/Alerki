061909d9b7c9c59ea13f446d2d873c3e458bd195
Petryk Oleksandr <sachapos50@gmail.com>
2022-06-05 00:05:03 +0300

    meta: update

--- api/config/jest.config.ts
+++ api/config/jest.config.ts
@@ -1,5 +1,8 @@
 export default {
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleNameMapper: {
+    '^@Config/(.*)$': '<rootDir>/config/$1',
+  },
   roots: ['./src', './test'],
   preset: 'ts-jest',
   testEnvironment: 'node',
