d816f4e89d700150626f1bda89e34c8f1fc73f3a
Petryk Oleksandr <sachapos50@gmail.com>
2022-07-15 16:07:35 +0300

    fix: set env variable decorator

--- api/src/shared/decorators/set-env-variable.decorator.ts
+++ api/src/shared/decorators/set-env-variable.decorator.ts
@@ -17,29 +17,34 @@ export enum SetAs {
  * @param type parse to type
  * @returns prepared and checked value
  */
-export function SetEnvVariable(variableName: string, type: SetAs = SetAs.string) {
-  const variable = process.env[variableName];
+export function SetEnvVariable(variableName: string, type: SetAs = SetAs.string): PropertyDecorator {
+  return (target: Record<string, any>, key: string | symbol) => {
+    const variable = process.env[variableName];
 
-  if (!variable) {
-    throw new Error(`${variableName} not exists, please set it in .env file`);
-  }
-
-  let parsed: string | number | any;
-
-  switch (type) {
-  case SetAs.string:
-    return variable;
-
-  case SetAs.number:
-    parsed = Number(variable);
-
-    if (isNaN(parsed)) {
-      throw new Error(`${variableName} is not a number, but we got ${variable}`);
+    if (!variable) {
+      throw new Error(`${variableName} not exists, please set it in the .env file`);
     }
 
-    return parsed;
+    let prepared: string | number | any;
 
-  default:
-    return variable;
-  }
+    switch (type) {
+    case SetAs.string:
+      prepared = variable;
+      break;
+
+    case SetAs.number:
+      prepared = Number(variable);
+
+      if (isNaN(prepared)) {
+        throw new Error(`${variableName} is not a number, but we got ${typeof variable}: ${variable}`);
+      }
+
+      break;
+
+    default:
+      throw new Error('Specified unhandled type');
+    }
+
+    target[key as string] = prepared;
+  };
 }
